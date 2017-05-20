15b3700995c7275c1a0ef717db0075a0534866b6
Mindskate <knyagowa@gmail.com>
2017-05-20 16:32:09 +0300

    tg webcalls main functions.py
    the get updates functions , sendmessage  and the likes to go here 
    the json content unloading functions too go here

---
+++ main funcs.py
@@ -0,0 +1,24 @@
+import json 
+import requests
+
+
+TOKEN = "Concealed" #you may request for it if you are interested in the project
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates():
+    url = URL + "getUpdates"
+    js = get_json_from_url(url)
+    return js
+
